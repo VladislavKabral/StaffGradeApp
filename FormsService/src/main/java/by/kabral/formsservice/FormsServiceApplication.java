bcbb35cb7b35061e5d720908fb32f288d31e8db6
Vladislav Kabral <mr.kabral@mail.ru>
2025-02-21 10:50:43 +0300

    Add Eureka Server and Eureka client configs

--- FormsService/src/main/java/by/kabral/formsservice/FormsServiceApplication.java
+++ FormsService/src/main/java/by/kabral/formsservice/FormsServiceApplication.java
@@ -2,8 +2,10 @@ package by.kabral.formsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class FormsServiceApplication {
 
   public static void main(String[] args) {
